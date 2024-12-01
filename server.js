8d0e5bcb4927b36af7d584449dfeb1d1cf72ab36
Zohair Ahmed <zohair.a99@gmail.com>
2024-12-01 03:34:25 -0500

    update PORT

--- server.js
+++ server.js
@@ -2,14 +2,11 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 5001;
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(process.env.PORT || 5000);
 
 const promptsFile = require('./data/prompts.json');
 const topicsFile = require('./data/topics.json');
