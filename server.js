fbaef513234a89c66f6d6001b508cb382603a8b1
Zohair Ahmed <zohair.a99@gmail.com>
2024-12-01 03:39:19 -0500

    update '/' endpoint, del 'api/random'

--- server.js
+++ server.js
@@ -12,24 +12,7 @@ const promptsFile = require('./data/prompts.json');
 const topicsFile = require('./data/topics.json');
 const axios = require('axios'); // Install axios for API requests: npm install axios
 
-app.get('/api/random', async (req, res) => {
-  try {
-    const randomTopic = topicsFile.topics[Math.floor(Math.random() * topicsFile.topics.length)];
-    const randomPrompt = promptsFile.prompts[Math.floor(Math.random() * promptsFile.prompts.length)];
-
-    // Fetch a random image
-    const imageResponse = await axios.get('https://picsum.photos/400/300', { responseType: 'arraybuffer' });
-    const base64Image = Buffer.from(imageResponse.data, 'binary').toString('base64');
-
-    res.json({
-      topic: randomTopic,
-      prompt: randomPrompt,
-      image: `data:image/jpeg;base64,${base64Image}`
-    })
-  } catch (error) {
-    res.status(500).send("Error generating random content.");
-  }
-});
+app.get('/');
 
 app.get('/api/prompt', async (_, res) => {
   try {
